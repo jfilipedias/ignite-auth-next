8eb43a043f6dc6f0c6e6689fbded97bc5d56c2e3
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-08-27 19:12:38 -0300

    Implements user authorization recovery from cookies

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
-import { setCookie } from "nookies";
+import { setCookie, parseCookies } from "nookies";
 
 import Router from "next/router";
 
@@ -33,23 +33,37 @@ const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+
+    if (token) {
+      api.get("/me").then((response) => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({ email, permissions, roles });
+      });
+    }
+  }, []);
+
   const signIn = async ({ email, password }: SignInCredentials) => {
     try {
       const response = await api.post("/sessions", { email, password });
 
       const { permissions, roles, token, refreshToken } = response.data;
 
-      setCookie(undefined, "next-auth-token", token, {
+      setCookie(undefined, "nextauth.token", token, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: "/",
       });
 
-      setCookie(undefined, "next-auth-refresh- token", refreshToken, {
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: "/",
       });
 
       setUser({ email, permissions, roles });
+
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
       Router.push("/dashboard");
     } catch (err) {
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,11 @@
 import axios from "axios";
+import { parseCookies } from "nookies";
 
-export const api = axios.create({ baseURL: "http://localhost:3333" });
+const cookies = parseCookies();
+
+export const api = axios.create({
+  baseURL: "http://localhost:3333",
+  headers: {
+    Authorization: `Bearer ${cookies["nextauth.token"]}`,
+  },
+});
